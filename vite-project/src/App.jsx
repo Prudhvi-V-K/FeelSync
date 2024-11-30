a905f16116176005989f6a2e78dcc6c01f396cab
prudhvi <mamillapalliprudhvi@gmail.com>
2024-11-30 19:38:18 +0530

    project setup

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function App() {
+  return (<>
+  Hello world
+  </>);
+}
+
